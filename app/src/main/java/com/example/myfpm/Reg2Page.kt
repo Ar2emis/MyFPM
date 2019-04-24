0a47c4c0d8d591f1faac4954d3fba2d9124bc28e
Dmitry Bereza <dmitrybereza2001@gmail.com>
2019-04-24 20:48:22 +0300

    4 try

---
+++ app/src/main/java/com/example/myfpm/Reg2Page.kt
@@ -0,0 +1,12 @@
+package com.example.myfpm
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class Reg2Page : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_reg2_page)
+    }
+}
