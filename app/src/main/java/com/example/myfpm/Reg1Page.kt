d3a5d3b1f4d7d0203ed088d0f9f9e944165e8999
Ar2emis <artemsheva0510@gmail.com>
2019-04-24 21:02:02 +0300

    DB was added

--- app/src/main/java/com/example/myfpm/Reg1Page.kt
+++ app/src/main/java/com/example/myfpm/Reg1Page.kt
@@ -4,12 +4,14 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import java.sql.Connection
 
 class Reg1Page : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_reg1_page)
+        val conn = ConnectionClass().Connect()
     }
 
     fun ObClick2 (view: View){
