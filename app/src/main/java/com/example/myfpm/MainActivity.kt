4d7b77260fd2627f87cdd6fd0d5b74855163455a
Dmitry Bereza <dmitrybereza2001@gmail.com>
2019-04-24 20:17:12 +0300

    2 try

--- app/src/main/java/com/example/myfpm/MainActivity.kt
+++ app/src/main/java/com/example/myfpm/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.example.myfpm
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun onClick (view: View){
+        val intent = Intent(this, Reg1Page::class.java )
+        startActivity(intent)
     }
 }
