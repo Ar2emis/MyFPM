c82d53cb5af6b8af7a3781031f4e4aa1ef554cc7
Dmitry Bereza <dmitrybereza2001@gmail.com>
2019-04-24 20:36:53 +0300

    3 try

--- app/src/main/java/com/example/myfpm/Reg1Page.kt
+++ app/src/main/java/com/example/myfpm/Reg1Page.kt
@@ -1,12 +1,19 @@
 package com.example.myfpm
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class Reg1Page : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_reg1_page)
+    }
+
+    fun ObClick2 (view: View){
+        val intent = Intent(this, Reg2Page::class.java )
+        startActivity(intent)
     }
 }
---
+++ app/src/main/java/com/example/myfpm/Reg2Page.java
@@ -0,0 +1,13 @@
+package com.example.myfpm;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Reg2Page extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reg2_page);
+    }
+}
