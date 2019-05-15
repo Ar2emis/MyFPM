8445e2aa8bff20f1e99c0a620b8e762a18a64e20
Dmitry Bereza <dmitrybereza2001@gmail.com>
2019-05-15 16:41:04 +0300

    Макет расписания

--- app/src/main/java/com/example/myfpm/TimeTableFragment.kt
+++ app/src/main/java/com/example/myfpm/TimeTableFragment.kt
@@ -7,6 +7,9 @@ import androidx.fragment.app.Fragment
 import  androidx.fragment.app.FragmentManager
 import android.view.ViewGroup
 import androidx.fragment.app.FragmentPagerAdapter
+import com.xwray.groupie.GroupAdapter
+import com.xwray.groupie.ViewHolder
+import kotlinx.android.synthetic.main.fragment_news.*
 import kotlinx.android.synthetic.main.fragment_time_table.*
 
 class TimeTableFragment : androidx.fragment.app.Fragment() {
@@ -22,35 +25,9 @@ class TimeTableFragment : androidx.fragment.app.Fragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        val adapter = MyViewPagerAdapter(activity?.supportFragmentManager!!)
-//        adapter.addFragment(MondayFragment(), "Monday")
-//        adapter.addFragment(TuesdayFragment(), "Tuesday")
-//        adapter.addFragment(WednesdayFragment(), "Wednesday")
-//        adapter.addFragment(ThursdayFragment(), "Thursday")
-//        adapter.addFragment(FridayFragment(), "Friday")
-        viewPagerTimeTable.adapter = adapter
-        tabs.setupWithViewPager(viewPagerTimeTable)
-    }
-}
 
-class MyViewPagerAdapter(manager: androidx.fragment.app.FragmentManager) : FragmentPagerAdapter(manager){
-    private val fragmentList : MutableList<Fragment> = ArrayList()
-    private val titleList : MutableList<String> = ArrayList()
+        val adapter = GroupAdapter<ViewHolder>()
+        rec_view_time_table.adapter = adapter
 
-    override fun getCount(): Int {
-       return fragmentList.size
-    }
-
-    override fun getItem(position: Int): Fragment {
-        return fragmentList[position]
-    }
-
-    fun addFragment (fragment: Fragment, title:String){
-        fragmentList.add(fragment)
-        titleList.add(title)
-    }
-
-    override fun getPageTitle(position: Int): CharSequence? {
-        return titleList[position]
     }
 }
